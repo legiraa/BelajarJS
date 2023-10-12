575166c4db839557332e1003c8a6ca5833782dcc
legiraa <legiras26@gmail.com>
2023-10-12 14:52:10 +0700

    Pembelajaran Pertemuan6.html dan JS

---
+++ main.js
@@ -0,0 +1,81 @@
+// console.log("Hello World")
+// alert("Notifikasi")
+// prompt("Siapa Namamu?")
+
+//terus harus satu-satu kalau mau var, var aja kalau mau let, let aja. Berlaku juga buat const
+
+// var Promnet = "Coding is Easy"
+// console.log(Promnet)
+// var Promnet = "Coding not Easy"
+// console.log(Promnet)
+
+// Promnet = "Coding is Easy"  //kalau ga ada tipe datanya maka diannggap var
+// console.log(Promnet)        
+
+// let Promnet = "Coding is Easy"   
+// console.log(Promnet)
+                                //kalauu let sama const cuman bisa satu , tidak sama seperti var
+// const Promnet = "Coding not Easy" 
+// console.log(Promnet)
+
+// let totalPoin = prompt("Masukan Poin Anda")
+//     if(totalPoin > ){
+//         document.write("Congratulations");
+//     }
+//     else{
+//         document.write("Yahh....");
+//     }
+
+// let x = 6;
+// let y = 3;
+
+// if (x < 10 && y > 1) {
+//   console.log("Kondisi a : True");
+// } else {
+//   console.log("Kondisi a : False");
+// }
+
+// if (x < 10 && y < 1) {
+//   console.log("Kondisi b : True");
+// } else {
+//   console.log("Kondisi b : False");
+// }
+
+// if (x == 5 || y == 5) {
+//   console.log("Kondisi c : True");
+// } else {
+//   console.log("Kondisi c : False");
+// }
+
+// if (x == 6 || y == 5) {
+//   console.log("Kondisi d : True");
+// } else {
+//   console.log("Kondisi d : False");
+// }
+
+// if (!(x == y)) {
+//   console.log("Kondisi e : True");
+// } else {
+//   console.log("Kondisi e : False");
+// }
+
+//Cara lainnya
+// let x = 6;
+// let y = 3;
+
+// console.log (x < 10 && y > 1)
+// console.log (x < 10 && y < 1)
+// console.log (x < 10 && y < 1)
+// console.log (x == 5 || y == 5)
+// console.log (x == 6 || y == 5)
+// console.log (x == y)
+
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
+
+// button.addEventListener('click', function(){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+// })
